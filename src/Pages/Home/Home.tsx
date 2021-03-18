f78ab0c7cfae67165f0e2fd326efc580388852eb
Anthony Nadaletti <anthony.nadaletti@gmail.com>
2021-03-18 19:48:27 -0300

    :sparkles: Adiciona navegação

---
+++ src/Pages/Home/Home.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+const Home: React.FC = () => {
+  
+  return (
+    <View style={{ flex: 1 }}>
+      <Text>
+        oi home
+      </Text>
+    </View>
+  )
+}
+
+export default Home
