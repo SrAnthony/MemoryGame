734a457236fd1c3e214b5a73c457dbbd52b9ec06
Anthony Nadaletti <anthony.nadaletti@gmail.com>
2021-03-23 00:22:56 -0300

    :hammer: Altera implementação de shuffle

--- src/Screens/Game/useRandomCards.ts
+++ src/Screens/Game/useRandomCards.ts
@@ -3,32 +3,20 @@ import { CardsList } from './Cards'
 
 export const CARDS_NUMBER = 10
 
+const shuffle = (arr: typeof CardsList) => (
+  arr.sort(() => Math.random() - 0.5)
+)
+
 // Eu randomizo a lista de todos os cards e pego CARDS_NUMBER deles
 // Depois eu duplico a lista e randomizo de novo
 const useRandomCards = (): typeof CardsList => {
   return useMemo(() => {
     const cards = shuffle(CardsList).slice(0, CARDS_NUMBER)
     
-    return shuffle([...cards, ...cards])
-      .map((card, i) => ({ ...card, key: `${card.key}-${i}` }))
+    let duplicated_cards = [...cards, ...cards].map((card, i) => ({ ...card, key: `${card.key}-${i}` }))
+    
+    return shuffle(duplicated_cards)
   }, [])
 }
 
 export default useRandomCards
-
-const shuffle = (arra1: any[]) => {
-  let ctr = arra1.length, temp, index
-  
-  // While there are elements in the array
-  while (ctr > 0) {
-    // Pick a random index
-    index = Math.floor(Math.random() * ctr)
-    // Decrease ctr by 1
-    ctr--
-    // And swap the last element with it
-    temp = arra1[ctr]
-    arra1[ctr] = arra1[index]
-    arra1[index] = temp
-  }
-  return arra1
-}
